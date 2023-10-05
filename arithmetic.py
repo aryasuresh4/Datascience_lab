47d3525c856a5ca1f398162ccdd3c8303cc32626
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2023-10-05 14:08:46 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,23 @@
+print("Demo of basic data types:Numbers")
+x=3
+y=2.5
+print("Data of variable x:",type(x))
+print("Data of variable y:",type(y))
+print("Addition:",x+y)
+print("Subtraction:",x-y)
+print("multiplication:",x*y)
+print("Exponentiation:",x**2)
+
+
+
+//
+C:\Users\SDPK_CEC\PycharmProjects\pythonProject3\venv\Scripts\python.exe C:\Users\SDPK_CEC\Desktop\pythonco2\arithmetic.py 
+Demo of basic data types:Numbers
+Data of variable x: <class 'int'>
+Data of variable y: <class 'float'>
+Addition: 5.5
+Subtraction: 0.5
+multiplication: 7.5
+Exponentiation: 9
+
+Process finished with exit code 0
