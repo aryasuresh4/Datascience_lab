28181312484d9859a40c149118b86bd87d97b031
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2023-10-05 15:02:51 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,16 @@
+print("Demo of function:program to find factorial of a number")
+def fact(n):
+    if n==1:
+        return 1
+    else:
+        return(n*fact(n-1))
+n=int(input("Enter a number:"))
+print("Factorial : ",fact(n))
+
+//
+
+Demo of function:program to find factorial of a number
+Enter a number:5
+Factorial :  120
+
+Process finished with exit code 0
