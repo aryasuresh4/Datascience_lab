c4480fadce20708708f7cc1699562562546411e6
aryasuresh4 <117814991+aryasuresh4@users.noreply.github.com>
2023-10-05 14:01:23 +0530

    Create boolean.py

---
+++ boolean.py
@@ -0,0 +1,24 @@
+print("Demo of basic data type:boolean")
+t=True
+f=False
+print("t=",t)
+print("f=",f)
+print("Data type of variable t:",type(t))
+print("Data type of variable f:", type(f))
+print("Logical AND operation:",t and f)
+print("logical OR operation:",t or f)
+print("logical NOT operation:",not t)
+print("logical XOR operation:",t!=f)
+
+//
+Demo of basic data type:boolean
+t= True
+f= False
+Data type of variable t: <class 'bool'>
+Data type of variable f: <class 'bool'>
+Logical AND operation: False
+logical OR operation: True
+logical NOT operation: False
+logical XOR operation: True
+
+Process finished with exit code 0
